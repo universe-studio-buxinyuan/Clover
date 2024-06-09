753e4bf7a45a1f239f955eedd790006b181a16af
buxinyuan <935160739@qq.com>
2024-06-10 03:14:19 +0800

    Add fucking player start implementation

--- Source/Clover/CoreSystem/PlayerStart/CloverPlayerStart.cpp
+++ Source/Clover/CoreSystem/PlayerStart/CloverPlayerStart.cpp
@@ -2,25 +2,95 @@
 
 
 #include "CloverPlayerStart.h"
+#include "GameFramework/GameModeBase.h"
 
 
-// Sets default values
-ACloverPlayerStart::ACloverPlayerStart()
+ACloverPlayerStart::ACloverPlayerStart(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer) {}
+
+ECloverPlayerStartLocationOccupancy ACloverPlayerStart::GetLocationOccupancy(AController* const ControllerPawnToFit) const
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	if (!HasAuthority())
+	{
+		return ECloverPlayerStartLocationOccupancy::Full;
+	}
+
+	UWorld* const World = GetWorld();
+	if (!World)
+	{
+		return ECloverPlayerStartLocationOccupancy::Full;
+	}
+	AGameModeBase* AuthGameMode = World->GetAuthGameMode();
+	if (!AuthGameMode)
+	{
+		return ECloverPlayerStartLocationOccupancy::Full;
+	}
+
+	const TSubclassOf<APawn> PawnClass = AuthGameMode->GetDefaultPawnClassForController(ControllerPawnToFit);
+	const APawn* const PawnToFit = PawnClass ? GetDefault<APawn>(PawnClass) : nullptr;
+
+	FVector ActorLocation = GetActorLocation();
+	const FRotator ActorRotation = GetActorRotation();
+
+	if (!World->EncroachingBlockingGeometry(PawnToFit, ActorLocation, ActorRotation, nullptr))
+	{
+		return ECloverPlayerStartLocationOccupancy::Empty;
+	}
+
+	if (World->FindTeleportSpot(PawnToFit, ActorLocation, ActorRotation))
+	{
+		return ECloverPlayerStartLocationOccupancy::Partial;
+	}
+
+	return ECloverPlayerStartLocationOccupancy::Full;
 }
 
-// Called when the game starts or when spawned
-void ACloverPlayerStart::BeginPlay()
+bool ACloverPlayerStart::IsClaimed() const
 {
-	Super::BeginPlay();
-	
+	return ClaimingController != nullptr;
 }
 
-// Called every frame
-void ACloverPlayerStart::Tick(float DeltaTime)
+bool ACloverPlayerStart::TryClaim(AController* OccupyingController)
 {
-	Super::Tick(DeltaTime);
+	if (OccupyingController == nullptr)
+	{
+		return false;
+	}
+
+	if (!IsClaimed())
+	{
+		return false;
+	}
+
+	ClaimingController = OccupyingController;
+	if (const UWorld* World = GetWorld())
+	{
+		const auto UpdateFunction = FTimerDelegate::CreateUObject(this, &ACloverPlayerStart::CheckUnclaimed);
+		World->GetTimerManager().SetTimer(ExpirationTimerHandle, UpdateFunction, ExpirationCheckInterval, true);
+	}
+	return true;
 }
 
+void ACloverPlayerStart::CheckUnclaimed()
+{
+	if (ClaimingController == nullptr)
+	{
+		return;
+	}
+
+	if (ClaimingController->GetPawn() == nullptr)
+	{
+		return;
+	}
+
+	if (GetLocationOccupancy(ClaimingController) != ECloverPlayerStartLocationOccupancy::Empty)
+	{
+		return;
+	}
+
+	ClaimingController = nullptr;
+	if (const UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(ExpirationTimerHandle);
+	}
+}
--- Source/Clover/CoreSystem/PlayerStart/CloverPlayerStart.h
+++ Source/Clover/CoreSystem/PlayerStart/CloverPlayerStart.h
@@ -22,5 +22,37 @@ class CLOVER_API ACloverPlayerStart : public APlayerStart
 	GENERATED_BODY()
 
 public:
-	ACloverPlayerStart(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	explicit ACloverPlayerStart(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	const FGameplayTagContainer& GetGameplayTags()
+	{
+		return StartPointTags;
+	}
+
+	ECloverPlayerStartLocationOccupancy GetLocationOccupancy(AController* const ControllerPawnToFit) const;
+
+	/** Did this player start get claimed by a controller already? */
+	bool IsClaimed() const;
+
+	/** If this PlayerStart was not claimed, claim it for ClaimingController */
+	bool TryClaim(AController* OccupyingController);
+
+protected:
+	/** Check if this PlayerStart is still claimed */
+	void CheckUnclaimed();
+
+	/** The controller that claimed this PlayerStart */
+	UPROPERTY(Transient)
+	TObjectPtr<AController> ClaimingController = nullptr;
+
+	/** Interval in which we'll check if this player start is not colliding with anyone anymore */
+	UPROPERTY(EditDefaultsOnly, Category = "Player Start Claiming")
+	float ExpirationCheckInterval = 1.f;
+
+	/** Tags to identify this player start */
+	UPROPERTY(EditAnywhere)
+	FGameplayTagContainer StartPointTags;
+
+	/** Handle to track expiration recurring timer */
+	FTimerHandle ExpirationTimerHandle;
 };
